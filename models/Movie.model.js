24122049fed46f729327f77c65d3b6f6256179d0
tahany777 <tahanymohammed777@hotmail.com>
2021-08-12 16:30:14 +0300

    add movies and weather

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -2,6 +2,7 @@
 
 class Movies {
   constructor(selected) {
+    this.key = selected.id;
     this.title = selected.original_title;
     this.vot = selected.vote_count;
     this.img = `https://image.tmdb.org/t/p/w500${selected.poster_path}`;
